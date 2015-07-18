078de8cc4908bb0f3829df00216fb96b5ac21695
ZoeAde <zoe.a.adelman@gmail.com>
2015-07-18 08:34:54 -0600

    adding framework to problemset

---
+++ js_logic.js
@@ -0,0 +1,17 @@
+//Prices (use while loops here)
+/*Users can see the sum of all prices
+Users can see the number of prices that are selected (the count)
+Users can see the sum of the selected prices
+Users can see the average of all of the prices*/
+
+
+
+
+
+
+
+
+
+
+
+//Rewrite all functions in js/logic.js to not use any for or while loops.
