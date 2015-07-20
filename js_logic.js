485012f09da7e155b460b5dd017a561f9f68c5f2
ZoeAde <zoe.a.adelman@gmail.com>
2015-07-19 19:01:42 -0600

    adding names.html file

--- js_logic.js
+++ js_logic.js
@@ -4,6 +4,21 @@ Users can see the number of prices that are selected (the count)
 Users can see the sum of the selected prices
 Users can see the average of all of the prices*/
 
+/*
+Hints for the first problem:
+  1. Define a function that:
+    - is assigned to a variable named `sum`
+    - has one parameter named `options`
+    - returns the calculated sum of all of the values of all of the options passed in
+NOTE: use a `for` loop to make this happen.
+*/
+
+function sum (options) {
+  for (var i = 0; i < options.length; i++) {
+    total = i+=
+    }
+    return total;
+  };
 
 
 
