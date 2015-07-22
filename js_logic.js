15bde1ed14ab258b55a7d4696baded6f66fe78f9
ZoeAde <zoe.a.adelman@gmail.com>
2015-07-21 18:54:10 -0600

    adding while loops

--- js_logic.js
+++ js_logic.js
@@ -1,32 +1,44 @@
-//Prices (use while loops here)
-/*Users can see the sum of all prices
-Users can see the number of prices that are selected (the count)
-Users can see the sum of the selected prices
-Users can see the average of all of the prices*/
-
-/*
-Hints for the first problem:
-  1. Define a function that:
-    - is assigned to a variable named `sum`
-    - has one parameter named `options`
-    - returns the calculated sum of all of the values of all of the options passed in
-NOTE: use a `for` loop to make this happen.
-*/
-
-function sum (options) {
-  for (var i = 0; i < options.length; i++) {
-    total = i+=
+//Users can see the sum of all prices
+var sumAll = function(options) {
+  var total = 0;
+    while (total < options.length) {
+    total += options;
     }
     return total;
   };
+sumAll();
 
+//Users can see the number of prices that are selected (the count)
+var numPrices = function(options) {
+  var count = 0
+    while (count < options.length) {
+    count += options;
+  }
+  return count;
+};
+numPrices();
 
+//Users can see the sum of the selected prices
+var selectedSum = function(options) {
+  var totalSelected = 0;
+  while (i < options.length) {
+    totalSelected += options;
+    }
+    return totalSelected;
+  };
+selectedSum();
 
-
-
-
-
-
+//Users can see the average of all of the prices
+var averagePrice = function(options) {
+  var total = 0;
+  var average = 0;
+  while (i < options.length) {
+    total += options;
+    average = total/options.length;
+    }
+  return average;
+};
+averagePrice();
 
 
 //Rewrite all functions in js/logic.js to not use any for or while loops.
