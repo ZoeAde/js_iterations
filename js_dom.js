4e5f30e9565a375f21beb6e2b8059126101b7a3d
ZoeAde <zoe.a.adelman@gmail.com>
2015-07-25 11:16:39 -0600

    adding name button functionality

--- js_dom.js
+++ js_dom.js
@@ -12,48 +12,131 @@ var answerBtn = document.getElementById('answer-box');
 
 var prices = document.getElementsByTagName('option');
 
-// //SumAll Values
-var sumAllClick = sumAllBtn.addEventListener("click", function(){
-var totalNums = document.getElementsByTagName("select")[0];
-var totalAnswer = sumAll(totalNums);
-answerBtn.innerHTML = totalAnswer;
+// // //SumAll Values
+// var sumAllClick = sumAllBtn.addEventListener("click", function(){
+// var totalNums = document.getElementsByTagName("select")[0];
+// var totalAnswer = sumAll(totalNums);
+// answerBtn.innerHTML = totalAnswer;
+// });
+
+// //CountSelected
+// var countSelectedClick = countSelectedBtn.addEventListener("click", function(){
+// var selectedNums = document.getElementsByTagName("select")[0];
+// var getSelectedOptions = selectedNums.selectedOptions;
+// console.log(getSelectedOptions);
+// var selectedAnswer = countPrices(getSelectedOptions);
+// console.log(selectedAnswer);
+//   answerBtn.innerHTML = selectedAnswer;
+// });
+
+// //SumSelected
+// var sumSelectedClick = sumSelectedBtn.addEventListener("click", function(){
+// var selectedNums = document.getElementsByTagName("select")[0];
+// var getSelectedOptions = selectedNums.selectedOptions;
+// console.log(getSelectedOptions);
+// var selectedAnswer = selectedSum(getSelectedOptions);
+// console.log(selectedAnswer);
+//   answerBtn.innerHTML = selectedAnswer;
+// });
+
+// //AverageAll
+// var averageAllClick = averageAllBtn.addEventListener("click", function(){
+// var allNums = document.getElementsByTagName("select")[0];
+// console.log(allNums);
+// var averageAnswer = averagePrice(allNums);
+// console.log(averageAnswer);
+//   answerBtn.innerHTML = averageAnswer;
+// });
+
+// //AverageSelected
+// var averageSelectedClick = averageSelectedBtn.addEventListener("click", function(){
+// var selectedNums = document.getElementsByTagName("select")[0];
+// var getSelectedOptions = selectedNums.selectedOptions;
+// console.log(getSelectedOptions);
+// var selectedAnswer = averagePrice(getSelectedOptions);
+// console.log(selectedAnswer);
+//   answerBtn.innerHTML = selectedAnswer;
+// });
+
+
+
+
+/////////////////////////NAMES//////////////////////
+
+var printAllBtn = document.getElementById('all-names');
+
+var printFirstBtn = document.getElementById('first-names');
+
+var printLastBtn = document.getElementById('last-names');
+
+var printLengthBtn = document.getElementById('names-with-lengths');
+
+
+
+
+
+
+//Converting Names To Array
+var textAreaNames= document.getElementsByTagName("textarea");
+var innerTextArea = textAreaNames[0].value;
+var textArray = innerTextArea.split("\n");
+
+
+//Printing all names
+var printAllClick = printAllBtn.addEventListener("click", function() {
+  for (var i = 0; i < textArray.length; i++) {
+    var answerUL = document.getElementById('answer-ul');
+    var li = document.createElement("li");
+    answerUL.appendChild(li).innerHTML = textArray[i];
+  }
 });
 
-//CountSelected
-var countSelectedClick = countSelectedBtn.addEventListener("click", function(){
-var selectedNums = document.getElementsByTagName("select")[0];
-var getSelectedOptions = selectedNums.selectedOptions;
-console.log(getSelectedOptions);
-var selectedAnswer = countPrices(getSelectedOptions);
-console.log(selectedAnswer);
-  answerBtn.innerHTML = selectedAnswer;
+//Printing First Name
+var printFirstClick = printFirstBtn.addEventListener("click", function() {
+
+  var firstWords = [];
+  for (var i = 0; i < textArray.length; i++) {
+    var textArrayed = textArray[i];
+    var firstWord = textArrayed.substr(0, textArrayed.indexOf(" "));
+    firstWords.push(firstWord);
+
+    var answerUL = document.getElementById('answer-ul');
+    var li = document.createElement("li");
+    answerUL.appendChild(li).innerHTML = firstWords[i];
+}
 });
 
-//SumSelected
-var sumSelectedClick = sumSelectedBtn.addEventListener("click", function(){
-var selectedNums = document.getElementsByTagName("select")[0];
-var getSelectedOptions = selectedNums.selectedOptions;
-console.log(getSelectedOptions);
-var selectedAnswer = selectedSum(getSelectedOptions);
-console.log(selectedAnswer);
-  answerBtn.innerHTML = selectedAnswer;
+//Printing Last Name
+var printLastClick = printLastBtn.addEventListener("click", function() {
+
+  var lastWords = [];
+  for (var i = 0; i < textArray.length; i++) {
+    var textArrayed = textArray[i];
+    var nameSplit = textArrayed.split(" ");
+    var lastWord = nameSplit[1];
+    lastWords.push(lastWord);
+    var answerUL = document.getElementById('answer-ul');
+    var li = document.createElement("li");
+    answerUL.appendChild(li).innerHTML = lastWords[i];
+}
 });
 
-//AverageAll
-var averageAllClick = averageAllBtn.addEventListener("click", function(){
-var allNums = document.getElementsByTagName("select")[0];
-console.log(allNums);
-var averageAnswer = averagePrice(allNums);
-console.log(averageAnswer);
-  answerBtn.innerHTML = averageAnswer;
+//Printing Name Lengths
+var printLengthClick = printLengthBtn.addEventListener("click", function() {
+  for (var i = 0; i < textArray.length; i++) {
+    var fullName = textArray[i];
+    var nameNoSpace = fullName.replace(/\s/g, "");
+    var nameLength = nameNoSpace.length;
+    var fullAndLength = [];
+    fullAndLength.push(fullName, nameLength);
+    var answerUL = document.getElementById('answer-ul');
+    var li = document.createElement("li");
+    answerUL.appendChild(li).innerHTML = fullAndLength;
+  }
 });
 
-//AverageSelected
-var averageSelectedClick = averageSelectedBtn.addEventListener("click", function(){
-var selectedNums = document.getElementsByTagName("select")[0];
-var getSelectedOptions = selectedNums.selectedOptions;
-console.log(getSelectedOptions);
-var selectedAnswer = averagePrice(getSelectedOptions);
-console.log(selectedAnswer);
-  answerBtn.innerHTML = selectedAnswer;
-});
+
+
+
+
+
